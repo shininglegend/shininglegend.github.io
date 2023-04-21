4964ab3fdb0799b4cfaa3191b2206b63ca538004
shininglegend <72107680+shininglegend@users.noreply.github.com>
2023-04-21 14:35:06 -0400

    Added blank versions of most pages

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import login_required, admin_required
+from helpers import login_required, admin_required, apology
 
 # Configure application
 app = Flask(__name__)
@@ -31,10 +31,68 @@ def after_request(response):
     return response
 
 
-# Login page
-@app.route("/")
-def index():
-    return render_template("index.html")
+# Homepage 
+# <ul class="navbar-nav me-auto mt-2">
+#     <li class="nav-item"><a class="nav-link" href="/">Your Journals</a></li>
+#     <li class="nav-item"><a class="nav-link" href="/journal">New Entry</a></li>
+# </ul>
+# <ul class="navbar-nav ms-auto mt-2">
+#     <li class="nav-item"><a class="nav-link" href="/logout">Log Out</a></li>
+# </ul>
+# {% else %}
+# <ul class="navbar-nav ms-auto mt-2">
+#     <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
+#     <li class="nav-item"><a class="nav-link" href="/login">Log In</a></li>
+# </ul>
+# {% endif %}
+# {% if session["admin"] %}
+# <ul>
+#     <li class="nav-item"><a class="nav-link" href="/client-journals">Journals</a></li>
+#     <li class="nav-item"><a class="nav-link" href="/clients">Clients</a></li>
+# </ul>
+
+# View Client's journals
+@app.route("/clients-journals")
+@login_required
+@admin_required
+def client_journals():
+    pass
+# Login page (stolen from my finance pset but like it was so well written :) 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide a valid password", 400)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
 
 # Journalling page
 
@@ -42,7 +100,3 @@ def index():
 # Response page
 
 
-# View the journals (admin)
-
-
-# 
--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import redirect, session
+from flask import redirect, session, render_template
 from functools import wraps
 
 # Configure CS50 Library to use SQLite database
@@ -20,11 +20,26 @@ def admin_required(f):
     # This checks for admin status
     # Loosely based off the code above
     # Check the user id
-    user_status = db.execute("SELECT adminstatus FROM users WHERE id=?", session.get("user_id"))[0]["adminstatus"]
+    #user_status = db.execute("SELECT adminstatus FROM users WHERE id=?", session.get("user_id"))[0]["adminstatus"]
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if user_status == False:
-            return redirect("/login")
-        return f(*args, **kwargs)
+        #if user_status == False:
+        return redirect("/login")
+        #return f(*args, **kwargs)
     return decorated_function
+
+# If you done messed up! (This is temporary)
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
